fc217e2d93cf40804cf07efaaaf488d8fbf70f1f
Hunter Hollenbeck <hunter@pumpkinseed.bio>
2024-05-19 20:20:06 -0700

    need to start back up at lij comp

--- src/test/scala/ProjectTemplate.scala
+++ src/test/scala/ProjectTemplate.scala
@@ -4,28 +4,68 @@ import spatial.dsl._
 
   type T = FixPt[TRUE,_10,_22]
 
-  val N = 1024  // A is NxN, and x is N wide.
+  val N = 1024  // A is NxN, and x is N wide
+  val d = 128
+  val M = 1024
+  //val Bc = M/(4*d)
+  //val Br = Math.min(Bc, d)
+  val Bc = 1
+  val Br = 1
+
 
   def main(args: Array[String]): Unit = {
 
     // These are on the HOST
-    val x_host = loadCSV1D[T](s"$DATA/vector.csv")
-    val A_host = loadCSV2D[T](s"$DATA/matrix.csv")
-    val A_dram = DRAM[T](N, N)
-    val x_dram = DRAM[T](N)
-    setMem(A_dram, A_host)
-    setMem(x_dram, x_host)
-    val out_host = DRAM[T](N)
+    val Q_host = loadCSV2D[T](s"$DATA/Q.csv")
+    val K_host = loadCSV2D[T](s"$DATA/K.csv")
+    val V_host = loadCSV2D[T](s"$DATA/V.csv")
+    val Q_dram = DRAM[T](N, d)
+    val K_dram = DRAM[T](N, d)
+    val V_dram = DRAM[T](N, d)
+    setMem(Q_dram, Q_host)
+    setMem(K_dram, K_host)
+    setMem(V_dram, V_host)
+    val l_dram = DRAM[T](N)
+    val O_dram = DRAM[T](N)
+    
     Accel {
-      val A_sram = SRAM[T](N, N)
-      val x_sram = SRAM[T](N)
+      val Q_sram = SRAM[T](Br, d)
+      val K_sram = SRAM[T](Bc, d)
+      val V_sram = SRAM[T](Bc, d)
+      val l_sram = SRAM[T](Br)
+      val lij_sram = SRAM[T](Br)
+      val O_sram = SRAM[T](Br, d)
+      val Sij_sram = SRAM[T](Br, Bc)
+      //A_sram load A_dram
+      //x_sram load x_dram
+      //val out_sram = SRAM[T](N)
+      Foreach(Tc by 1) {j=>
+	K_sram load K_dram(j*Bc::(j+1)*Bc, 0::d)
+	V_sram load V_dram(j*Bc::(j+1)*Bc, 0::d)
+	Foreach(Tr by 1) {i =>
+	  Q_sram load Q_dram(i*Br::(i+1)*Br, 0::d)
+	  O_sram load O_dram(i*Br::(i+1)*Br, 0::d)
+	  l_sram load l_dram(i*Br::(i+1)*Br) 
+	  
+	  //calculate Sij
+	  MemFold(Sij_sram)(d by 1){m=>
+	    val tmp = SRAM[T](Br, Bc)
+	    Foreach(Br by 1, Bc by 1){ (n,o)=>
+	      tmp(n,o) = Q_sram(n, m) * K_sram(o, m)
+	    }tmp
+	  }{_+_}
+	  //done with Sij
+	  //calculate Pij (in Sij)
+	  Foreach(Br by 1, Bc by 1){ (n,o)=>
+	      Sij_sram(n,o) = exp(Sij_sram(n,o))
+	  }
+	  //done with Pij
+	  //start lij
+	  MemFold(li
 
-      A_sram load A_dram
-      x_sram load x_dram
 
-      val out_sram = SRAM[T](N)
-      Foreach(0 until N) {
-        i =>
+	  } 
+
           val accum = Reg[T]
           Reduce(accum)(0 until N) {
             j => A_sram(i, j) * x_sram(j)
