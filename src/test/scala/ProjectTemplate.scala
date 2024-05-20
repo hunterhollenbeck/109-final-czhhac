d8fa28093505112dfb3ed0c5402b4b574c403264
Hunter Hollenbeck <hunter@pumpkinseed.bio>
2024-05-20 10:35:54 -0700

    done, add brackets, test data, etc

--- src/test/scala/ProjectTemplate.scala
+++ src/test/scala/ProjectTemplate.scala
@@ -26,7 +26,7 @@ import spatial.dsl._
     setMem(K_dram, K_host)
     setMem(V_dram, V_host)
     val l_dram = DRAM[T](N)
-    val O_dram = DRAM[T](N)
+    val O_dram = DRAM[T](N, d)
     
     Accel {
       val Q_sram = SRAM[T](Br, d)
@@ -34,8 +34,10 @@ import spatial.dsl._
       val V_sram = SRAM[T](Bc, d)
       val l_sram = SRAM[T](Br)
       val lij_sram = SRAM[T](Br)
+      val lnew_sram = SRAM[T](Br)
       val O_sram = SRAM[T](Br, d)
       val Sij_sram = SRAM[T](Br, Bc)
+      val PijVj_sram = SRAM[T](Br, d)
       //A_sram load A_dram
       //x_sram load x_dram
       //val out_sram = SRAM[T](N)
@@ -46,7 +48,7 @@ import spatial.dsl._
 	  Q_sram load Q_dram(i*Br::(i+1)*Br, 0::d)
 	  O_sram load O_dram(i*Br::(i+1)*Br, 0::d)
 	  l_sram load l_dram(i*Br::(i+1)*Br) 
-	  
+	
 	  //calculate Sij
 	  MemFold(Sij_sram)(d by 1){m=>
 	    val tmp = SRAM[T](Br, Bc)
@@ -61,11 +63,35 @@ import spatial.dsl._
 	  }
 	  //done with Pij
 	  //start lij
-	  MemFold(li
-
-
-	  } 
-
+	  MemFold(lij_sram)(Bc by 1){m =>
+	    val tmp = SRAM[T](Br)
+	    Foreach(Br by 1){n =>
+	      tmp(n) = Sij_sram(m, n)
+ 	    }tmp
+	  }{_+_} 
+	  //end lij
+	  //start lnew
+	  Foreach(Br by 1){m =>
+	    lnew_sram(m) = l_sram(m) + lij_sram(m)
+	  }
+	  //end lnew
+	  //store l
+	  l_dram(i*Br::(i+1)*Br) store lnew_sram
+	  
+	  //compute P_ijV_j
+	  MemFold(PijVj_sram)(Bc by 1){m=>
+	    val tmp = SRAM[T](Br, d)
+	    Foreach(Br by 1, d by 1){ (n,o)=>
+	      tmp(n,o) = Sij_sram(n, m) * V_j_sram(o, m)
+	    }tmp
+	  }{_+_}
+	  //done compute P_ijV_j
+	  //begin Oi
+	  Foreach(Br by 1, d by 1){(n, o)=>
+	    Oi_sram(n,o) = (l_sram(n) * Oi_sram(n,o) + PijVj_sram(n,o))/lnew_sram(n)
+	  }
+	  O_dram(i*Br::(i+1)*Br, 0::d) store Oi_sram
+	//I THINK IT's DONE EXCEPT FOR THE WRITEBACK STEP
           val accum = Reg[T]
           Reduce(accum)(0 until N) {
             j => A_sram(i, j) * x_sram(j)
